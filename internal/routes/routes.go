271df8c3213f4fa954cc9fbb3708a8acb9135438
Abdullah Al Khalid <khalidrafi1111@gmail.com>
2025-08-31 17:31:57 +0000

    update: checking ports or dev env

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,5 +9,10 @@ func Routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /upload", handlers.UploadHandler)
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("Hello there!"))
+	})
 	return mux
 }
